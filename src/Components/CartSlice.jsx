b5097941254e79a40abac67460b137a59d2467cf
HASSAN-HKH <hassankhalaf409@gmail.com>
2025-09-07 11:44:49 -0400

    First Draft

--- src/Components/CartSlice.jsx
+++ src/Components/CartSlice.jsx
@@ -1,6 +1,47 @@
-
-const CartSlice = ({
- 
+import { createSlice } from '@reduxjs/toolkit';
+const CartSlice = createSlice({
+    name:"cart",
+    initialState,
+    reducers: {
+        addItemToCart: (state , action) => {
+            const isExisted = state.cartItems.find((item) => item.id === action.payload.id)
+            if(isExisted){
+                isExisted.quantity += 1;
+            }
+            else{
+                state.cartItems.push({...action.payload , quantity:1})
+            }
+        },
+        removeItemFromCart: (state , action) => {
+            state.cartItems = state.cartItems.filter((item) => item.id !== action.payload.id);
+        },
+        clearCart: (state) => {
+            state.cartItems = [];
+        },
+        increaseItemQuantity: (state , action) => {            
+            const isExisted = state.cartItems.find((item) => item.id === action.payload.id)
+            if(isExisted){
+                isExisted.quantity += 1;
+            }
+        },
+        decreaseItemQuantity: (state , action) => {            
+            const isExisted = state.cartItems.find((item) => item.id === action.payload.id)
+            if(isExisted && isExisted.quantity > 1){
+                isExisted.quantity -= 1;
+            }
+        }
+    }
 });
 
+const initialState = {
+    cartItems:[]
+}
 
+export const {
+    addItemToCart,
+  removeItemFromCart,
+  clearCart,
+  increaseItemQuantity,
+  decreaseItemQuantity,
+} = CartSlice.actions;
+export default CartSlice.reducer
--- src/Components/ProductList.jsx
+++ src/Components/ProductList.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './ProductList.css'; 
-
+ 
 const ProductList = () => {
 
   const products = [
@@ -13,7 +13,18 @@ const ProductList = () => {
     <div className="product-list">
       <h2 className="product-list-title">Products</h2>
       <ul className="product-list-items">
-     
+        {products.map((product) => {
+            return(
+                <>
+                    <li key={product.id} className="product-list-item">
+                        <span>{product.name} - ${product.price}</span>
+                        <button>
+                            Add to cart
+                        </button>
+                    </li>
+                </>
+            )
+        })}
       </ul>
     </div>
   );
